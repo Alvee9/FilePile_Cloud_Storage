d796bcf37dce4a8e200386a86ac4a2603f712046
Alvee9 <abdullah.alvee@gmail.com>
2018-04-20 21:22:45 +0600

    firstone

--- filepile/firstOne/models.py
+++ filepile/firstOne/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import mysql.connector
 
 # Create your models here.
+
+
+class ConnectDB:
+    def __init__(self):
+        self.conn = mysql.connector.connect(host='localhost', database='file_pile', user='root', password='')
+
+    def DBVar(self):
+        return self.conn
+
+
+class ValidateUser:
+    def __init__(self):
+        a = 10
+
+    def check(self, username, password):
+        # print(username, password)
+        conn = ConnectDB()
+        conn = conn.DBVar()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM USERS")
+        rows = cursor.fetchall()
+        for row in rows:
+            #print(row)
+            if row[1] == username and row[3] == password:
+                conn.close()
+                return int(row[0])
+        conn.close()
+        return -1
+
--- filepile/firstOne/views.py
+++ filepile/firstOne/views.py
@@ -21,3 +21,18 @@ def folder(request):
 
 def upload(request):
     return render(request, 'folder.html', {})
+
+
+def authenticate(request):
+    params = request.POST
+    v = ValidateUser()
+    check = v.check(params['email'], params['password'])
+    tmp1 = {'username': params['email'], 'password': params['password'], 'session_user': -1}
+
+    if check != -1:
+        request.session['user'] = check
+        tmp1['session_user'] = check
+        return render(request, 'landing.html', {'tmp1': tmp1})
+    else:
+        request.session['user'] = -1
+        return render(request, 'landing.html', {'tmp1': tmp1})
