c2ae211d0cc2833ced32028530ce5cbd0769252f
Alvee9 <abdullah.alvee@gmail.com>
2018-09-30 19:42:33 +0600

    issues regarding .gitignore resolved

--- filepile/filepile/settings.py
+++ filepile/filepile/settings.py
@@ -78,6 +78,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'file_pile',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'DEFAULT-CHARACTER-SET': 'utf8',
+
     }
 }
 
